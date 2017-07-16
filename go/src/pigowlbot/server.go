79e7ee0f6afb55a278fc7b45010338a2a290e94b
Dmitry Fomin <fomin.dm@gmail.com>
2017-07-17 02:11:23 +0300

    Port fix.

--- go/src/pigowlbot/server.go
+++ go/src/pigowlbot/server.go
@@ -18,13 +18,13 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.pigowl.com:8444/"+bot.Token, "fullchain.pem"))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.pigowl.com:8443/"+bot.Token, "fullchain.pem"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS(":8444", "fullchain.pem", "privkey.pem", nil)
+	go http.ListenAndServeTLS(":8443", "fullchain.pem", "privkey.pem", nil)
 
 	for update := range updates {
 		log.Printf("%+v\n", update)
