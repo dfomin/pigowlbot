c551a3e95ce22f19301f9ccce9f66cd5fd5696ce
Dmitry Fomin <fomin.dm@gmail.com>
2017-07-23 01:19:22 +0300

    s

---
+++ go/src/pigowlbot/private/private.go
@@ -0,0 +1,10 @@
+package private
+
+// Telegram token
+const (
+	BotToken = "180899935:AAGnzCdrQdgq9aoO8UnX8s9-cefjz2TB9ho"
+
+	GetPacksUrl     = "http://pigowl.com:8080/getPacks"
+	GetPacksStatUrl = "http://pigowl.com:8090/getPacksStat"
+	GetGamesUrl     = "http://pigowl.com:8090/getGames"
+)
--- go/src/pigowlbot/server.go
+++ go/src/pigowlbot/server.go
@@ -3,13 +3,58 @@ package main
 import (
 	"log"
 	"net/http"
-	"pigowlbot/token"
+	"pigowlbot/api"
+	"pigowlbot/private"
+	"pigowlbot/sort"
+	"strconv"
+	"strings"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
 
+func getPackages() string {
+	response := api.GetPackages()
+
+	var parts []string
+	for _, pack := range response.Packs {
+		parts = append(parts, pack.Pack.Name)
+	}
+	return strings.Join(parts, "\n")
+}
+
+func getPackagesName() map[int]string {
+	packageIdNameMap := make(map[int]string)
+
+	packsResponse := api.GetPackages()
+	for _, pack := range packsResponse.Packs {
+		packageIdNameMap[pack.Pack.ID] = pack.Pack.Name
+	}
+	return packageIdNameMap
+}
+
+func getDownloads(period int64) string {
+	packageIdNameMap := getPackagesName()
+	packsStatResponse := api.GetPackagesStatistics()
+
+	downloadsMap := make(map[string]int)
+	for _, packStat := range packsStatResponse.PacksStat {
+		if packStat.Timestamp >= period {
+			downloadsMap[packageIdNameMap[packStat.ID]]++
+		}
+	}
+
+	var result []string
+	for _, v := range sort.SortedKeys(downloadsMap) {
+		result = append(result, v+", "+strconv.Itoa(downloadsMap[v]))
+	}
+	if len(result) > 0 {
+		return strings.Join(result, "\n")
+	}
+	return "There were not any downloads :'("
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI(token.BotToken)
+	bot, err := tgbotapi.NewBotAPI(private.BotToken)
 	if err != nil {
 		log.Fatal(err)
 	}
