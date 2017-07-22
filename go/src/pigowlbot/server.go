9ae3ae2a64f2a6c0c42227712689734af8254421
Dmitry Fomin <fomin.dm@gmail.com>
2017-07-22 18:43:11 -0400

    Fixes.

--- go/src/pigowlbot/server.go
+++ go/src/pigowlbot/server.go
@@ -60,17 +60,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
+	//bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://pigowl.com:8443/"))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://pigowl.com:88/"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	updates := bot.ListenForWebhook("/")
-	go http.ListenAndServeTLS(":8443", "fullchain.pem", "privkey.pem", nil)
+	go http.ListenAndServeTLS(":88", "fullchain.pem", "privkey.pem", nil)
 
 	for update := range updates {
 		command := update.Message.Command()
